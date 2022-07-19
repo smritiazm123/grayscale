0d829a4af82cca5bfaf9e45940e19fd0eda303d6
smritiazm123 <87638544+smritiazm123@users.noreply.github.com>
2022-07-19 12:39:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function f_button1()
+        {
+            var i=document.getElementById("image");
+            i.style.filter+="grayscale(10%)";
+            
+        }
+        function f_button2()
+        {
+            var i=document.getElementById("image");
+            i.style.filter-="grayscale(10%)";
+            /*if(j.style.filter=="grayscale(0%)")
+            {
+                console.log("Grayscale 0%");
+            }*/
+        }
